19f09a6e5bab25bcacbfaf29927915b3a6dcfb71
coder178 <pateldrashti1708@gmail.com>
2022-09-18 18:45:49 +0530

    lab10_tutorial1

---
+++ lab10_tutorial1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lab10_tutorial1/pages/choose_location.dart';
+import 'package:lab10_tutorial1/pages/home.dart';
+import 'package:lab10_tutorial1/pages/loading.dart';
+
+
+void main() => runApp(MaterialApp(
+  // home: Home(),
+  // initialRoute: '/home',
+  initialRoute: '/',
+
+  routes: {
+    '/' : (context) => Loading(),
+    '/home' : (context) => Home(),
+    '/location' : (context) => ChooseLocation()
+  },
+));
