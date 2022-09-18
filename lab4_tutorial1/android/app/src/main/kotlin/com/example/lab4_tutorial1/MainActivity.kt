897bda4975528f1769d9a48bcc7cbe4dd34dd381
coder178 <pateldrashti1708@gmail.com>
2022-09-18 20:02:10 +0530

    lab 4, 5, 6

---
+++ lab4_tutorial1/android/app/src/main/kotlin/com/example/lab4_tutorial1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab4_tutorial1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab4_tutorial2/android/app/src/main/kotlin/com/example/lab4_tutorial2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab4_tutorial2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
