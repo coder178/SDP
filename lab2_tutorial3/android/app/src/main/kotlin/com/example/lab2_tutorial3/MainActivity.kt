f5a1ad8cc186e2276acd61721f482c6fc1c8ce2d
coder178 <pateldrashti1708@gmail.com>
2022-09-18 19:22:24 +0530

    lab2

---
+++ lab2_tutorial3/android/app/src/main/kotlin/com/example/lab2_tutorial3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab2_tutorial3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab2_tutorial4/android/app/src/main/kotlin/com/example/lab2_tutorial4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab2_tutorial4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
